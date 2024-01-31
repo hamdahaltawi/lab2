3c042909d610feefb88d98be18d06d692e8ce9f2
Hamdah <tony-altawi@hotmail.com>
2024-01-31 11:28:08 +0300

    simple print statment

---
+++ src/lab2251/Lab2251.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2251;
+
+/**
+ *
+ * @author Tony-
+ */
+public class Lab2251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("helloe world ");
+    }
+    
+}
